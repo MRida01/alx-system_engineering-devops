62ad05a13f1e79d09e4e1f232e07eb9656ea331e
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-04-24 07:06:25 -0700

    updates

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,5 @@
 #Installs flask from pip3
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => ['/usr/bin'],
-  refreshonly => true,
-  subscribe   => Package['python3-pip'],
-}
-
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
-  require  => Exec['install_flask'],
+  provider => 'pip3',
 }
