6ce5581022d16b96e4748061924a4ebadc443ef6
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-04-24 20:01:08 -0700

    updates

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Configures SSH server
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/sshd_config',
+  line    => 'PasswordAuthentication no',
+  ensure  => present,
+  match   => '^PasswordAuthentication.*$',
+}
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  ensure  => present,
+  match   => '^#?IdentityFile.*$',
+}
